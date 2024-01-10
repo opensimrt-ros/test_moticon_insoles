171d53f529817425461837a538ea33f7fd09aca3
frekle <frekle@kth.se>
2024-01-10 18:23:29 +0000

    shows insoles with some simple shape

---
+++ demo_only_insoles_no_imu.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+SESSION_NAME=demo
+
+source "`rospack find tmux_session_core`/common_functions.bash"
+ros_core_tmux "$SESSION_NAME"
+
+tmux set -g pane-border-status top
+
+W1=(
+"roslaunch moticon_insoles read_sdk.launch --wait"
+"rqt_plot"
+"roslaunch republisher republisher_insoles.launch --wait"
+"roslaunch moticon_insoles show_urdf.launch"
+)
+
+create_tmux_window "$SESSION_NAME" "main_nodes" "${W1[@]}"
+
+tmux -2 a -t $SESSION_NAME
