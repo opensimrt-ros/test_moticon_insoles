59c3ba7056ecec6e609c0cbd75f19c4fc0600582
frederico-klein <frekle@kth.se>
2024-04-20 13:41:52 +0200

    shared location for files

--- test/test_moticon_insole.py
+++ test/test_moticon_insole.py
@@ -10,7 +10,7 @@ class TestMoticonInsole(unittest.TestCase):
         import insoles_common
         #rospy.init_node("test_insole_sdk")
         insrv = insoles_common.InsoleSrv()
-        getter = insoles_common.InsoleDataFromFile(filename = "/catkin_ws/Data/ruoli/ViconData/Ruoli/Moticon_insole/RealTimekIDS2/walking01_header_corrected.txt")
+        getter = insoles_common.InsoleDataFromFile(filename = "/srv/host_data/ruoli/ViconData/Ruoli/Moticon_insole/RealTimekIDS2/walking01_header_corrected.txt")
         insrv.set_getter(getter)
         insrv.waiting = False
         self.assertEqual(True,True) ## test
